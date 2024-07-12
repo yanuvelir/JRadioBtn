bbad76d3ab4c25e544fda9fd9a719ff9c74aea5d
yanuvelir <yanuvelir@gmail.com>
2024-07-12 17:41:33 +0300

    completed

--- src/JRadioBtn.java
+++ src/JRadioBtn.java
@@ -1,2 +1,12 @@
-package PACKAGE_NAME;public class JRadioBtn {
+import javax.swing.*;
+
+public class JRadioBtn {
+
+    public static void main(String[] args){
+
+        Window win = new Window("Window - JRadioButton");
+        win.setDefaultCloseOperation(JFrame.EXIT_ON_CLOSE);
+        win.setSize(350, 200);
+        win.setVisible(true);
+    }
 }
--- src/Window.java
+++ src/Window.java
@@ -1,2 +1,56 @@
-package PACKAGE_NAME;public class Window {
+import javax.swing.*;
+import java.awt.*;
+import java.awt.event.*;
+
+public class Window extends JFrame{
+
+    private JTextField txtText;
+    private Font fntPlain, fntBold, fntItalic, fntBoldItalic;
+    private JRadioButton rdbPlain, rdbBold, rdbItalic, rdbBoldItalic;
+    private ButtonGroup btnGrpFonts;
+
+    public Window(String header){
+        super(header);
+        this.setLayout(new FlowLayout());
+        txtText = new JTextField("To change the font - please select radio button", 30);
+        this.add(txtText);
+
+        fntPlain = new Font("TimesRoman", Font.PLAIN, 14);
+        fntBold = new Font("TimesRoman", Font.BOLD, 14);
+        fntItalic = new Font("TimesRoman", Font.ITALIC, 14);
+        fntBoldItalic = new Font("TimesRoman", Font.BOLD + Font.ITALIC, 14);
+
+        rdbPlain = new JRadioButton("Regular", true);
+        add(rdbPlain);
+        rdbBold = new JRadioButton("Bold", false);
+        add(rdbBold);
+        rdbItalic = new JRadioButton("Italic", false);
+        add(rdbItalic);
+        rdbBoldItalic = new JRadioButton("BoldItalic");
+        add(rdbBoldItalic);
+
+        btnGrpFonts = new ButtonGroup();
+        btnGrpFonts.add(rdbPlain);
+        btnGrpFonts.add(rdbBold);
+        btnGrpFonts.add(rdbItalic);
+        btnGrpFonts.add(rdbBoldItalic);
+
+        rdbPlain.addItemListener(new RdbEventHandlerClass(fntPlain));
+        rdbBold.addItemListener(new RdbEventHandlerClass(fntBold));
+        rdbItalic.addItemListener(new RdbEventHandlerClass(fntItalic));
+        rdbBoldItalic.addItemListener(new RdbEventHandlerClass(fntBoldItalic));
+    }
+
+    private class RdbEventHandlerClass implements ItemListener {
+        private Font fnt;
+        public RdbEventHandlerClass(Font f){
+            fnt = f;
+        }
+        public void itemStateChanged(ItemEvent e){
+            txtText.setFont(fnt);
+        }
+
+    }
+
 }
+
